49ec1a14e0950b80a1a3e745c3e70cd5277e2180
skykanin <3789764+skykanin@users.noreply.github.com>
2018-05-06 11:58:26 +0200

    Fixed borders for crypto elements

--- app/src/main/java/skykanin/cryptovalue/MainActivityUI.kt
+++ app/src/main/java/skykanin/cryptovalue/MainActivityUI.kt
@@ -1,49 +1,73 @@
 package skykanin.cryptovalue
 
-import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.res.ResourcesCompat
 import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.TEXT
 import org.jetbrains.anko.design.longSnackbar
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivityUI : AnkoComponent<MainActivity> {
+
     override fun createView(ui: AnkoContext<MainActivity>): View = with(ui) {
-        frameLayout {
-            val textField = editText {
-                hint="Text for toasts and snackbars"
-            }.lparams(width = matchParent) {
-                margin = dip(12)
-                topMargin = dip(30)
-            }
-
-            imageView(R.drawable.ic_android_black_24dp) {
-                onClick {
-                    this@imageView.imageTintList =
-                            ColorStateList.valueOf(Color.parseColor("#4CAF50"))
-                }
-            }.lparams(dip(72), dip(72)) {
-                gravity = Gravity.CENTER
-            }
-
-            linearLayout {
-                button("Show toast") {
-                    onClick {
-                        toast(textField.text)
+        scrollView {
+            verticalLayout {
+                linearLayout {
+                    imageView(R.drawable.btc).lparams(dip(75), dip(75)) {
+                        rightMargin = dip(20)
                     }
+
+                    textView {
+                        text = "BTC"
+                        textSize = 24f
+                        textColor = Color.BLACK
+                    }.lparams {
+                        gravity = Gravity.CENTER_VERTICAL
+                    }
+
+                    textView {
+                        text = "$ 7320"
+                        textSize = 24f
+                    }.lparams {
+                        gravity = Gravity.CENTER_VERTICAL
+                        leftMargin = dip(100)
+                    }
+
+                    background = ResourcesCompat.getDrawable(resources, R.drawable.border_new, null)
+                }.lparams(width = matchParent, height = dip(100)) {
+                    gravity = Gravity.START
                 }
 
-                button("Show snackbar") {
-                    onClick {
-                        longSnackbar(this@frameLayout, textField.text.toString())
+                linearLayout {
+                    imageView(R.drawable.btc).lparams(dip(75), dip(75)) {
+                        rightMargin = dip(20)
                     }
+
+                    textView {
+                        text = "BTC"
+                        textSize = 24f
+                        textColor = Color.BLACK
+                    }.lparams {
+                        gravity = Gravity.CENTER_VERTICAL
+                    }
+
+                    textView {
+                        text = "$ 7320"
+                        textSize = 24f
+                    }.lparams {
+                        gravity = Gravity.CENTER_VERTICAL
+                        leftMargin = dip(100)
+                    }
+
+                    background = ResourcesCompat.getDrawable(resources, R.drawable.border_new, null)
+                }.lparams(width = matchParent, height = dip(100)) {
+                    gravity = Gravity.START
                 }
-            }.lparams {
-                gravity = Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL
-                bottomMargin= dip(72)
-            }
+            }.lparams(width= matchParent, height = matchParent)
         }
     }
-
 }
